389e130df4a5d68a3253d64645c0176c330e0502
Sam Bernal <sambernaldamasco@gmail.com>
2020-01-06 01:28:08 -0500

    little tweak

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = Team.find(user_params[:team_id]).users.find_by(username: user_params[:username])
-
+    @errormsg = "wrong username or password"
     if @user.authenticate(user_params[:password])
       render json: @user, :except => [:created_at, :updated_at, :password_digest]
     else
-      render :nothing => true, :status => 204
+      render json: @errormsg, status: :unprocessable_entity
     end
   end
 
